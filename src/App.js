310b8cde006f226b8980e44c6b11e3641fa3a298
Devin Chen <d3vin.chen@gmail.com>
2019-07-03 00:18:12 +0800

    Install recompose

--- src/App.js
+++ src/App.js
@@ -8,13 +8,7 @@ function App() {
   }
   return (
     <div className="App">
-      <MyMapComponent
-        isMarkerShown
-        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`}
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `400px` }} />}
-        mapElement={<div style={{ height: `100%` }} />}
-      />
+      <MyMapComponent apiKey={apiKey} isMarkerShown />
     </div>
   );
 }
--- src/MyMapComponent.jsx
+++ src/MyMapComponent.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { compose, withProps } from "recompose";
 import {
   withScriptjs,
   withGoogleMap,
@@ -6,14 +7,23 @@ import {
   Marker
 } from "react-google-maps";
 
-const MyMapComponent = withScriptjs(
-  withGoogleMap(props => (
-    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-      {props.isMarkerShown && (
-        <Marker position={{ lat: -34.397, lng: 150.644 }} />
-      )}
-    </GoogleMap>
-  ))
-);
+const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+
+const MyMapComponent = compose(
+  withProps(ownerProps => ({
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`,
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />
+  })),
+  withScriptjs,
+  withGoogleMap
+)(props => (
+  <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+    {props.isMarkerShown && (
+      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+    )}
+  </GoogleMap>
+));
 
 export default MyMapComponent;
