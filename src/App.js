1f5ef1fab212bf8bcdd7b797c7841fa910084241
Devin Chen <d3vin.chen@gmail.com>
2019-07-03 00:35:19 +0800

    Implement state transition logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MyMapComponent from "./MyMapComponent";
 
-function App() {
+function App(props) {
+  const [isMarkerShown, setIsMarkerShown] = useState(false);
+
+  const delayedShowMarker = () => {
+    setTimeout(() => {
+      setIsMarkerShown(true);
+    }, 3000);
+  };
+
+  const handleMarkerClick = () => {
+    setIsMarkerShown(false);
+    delayedShowMarker();
+  };
+
+  useEffect(() => {
+    delayedShowMarker();
+    console.log("mount it!");
+  }, []);
+
   const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
   if (!apiKey) {
     return <div>Please fill your Google Maps API key in .env file</div>;
   }
   return (
     <div className="App">
-      <MyMapComponent apiKey={apiKey} isMarkerShown />
+      <MyMapComponent
+        apiKey={apiKey}
+        isMarkerShown={isMarkerShown}
+        onMarkerClick={handleMarkerClick}
+      />
     </div>
   );
 }
--- src/MyMapComponent.jsx
+++ src/MyMapComponent.jsx
@@ -7,11 +7,11 @@ import {
   Marker
 } from "react-google-maps";
 
-const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-
 const MyMapComponent = compose(
   withProps(ownerProps => ({
-    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`,
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${
+      ownerProps.apiKey
+    }&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />
@@ -21,7 +21,10 @@ const MyMapComponent = compose(
 )(props => (
   <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
     {props.isMarkerShown && (
-      <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      <Marker
+        position={{ lat: -34.397, lng: 150.644 }}
+        onClick={props.onMarkerClick}
+      />
     )}
   </GoogleMap>
 ));
